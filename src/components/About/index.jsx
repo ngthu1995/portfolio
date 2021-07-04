9fc29b0c4060f736a1e6c860f38f61ad732fa50b
ngthu1995 <ngthu1995@gmail.com>
2021-07-04 08:15:56 -0400

    update intro

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -27,7 +27,7 @@ const About = () => {
         </Grid>
         <Grid item className={classes.text}>
           <Typography variant="body1">
-            Hi, I am Tina, a 2-year-developer with background of 6 years
+            Hi, I am Tina, a 3-year-developer with background of 6 years
             experience in design. I write data migration, build reusable code
             and libraries, graphic intensive web applications, develop new
             user-facing features and functionalities with user experience in
