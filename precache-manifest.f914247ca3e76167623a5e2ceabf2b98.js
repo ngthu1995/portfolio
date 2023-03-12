67f83c78ac2160f5c5fcbccff6b94fcd51ce5476
ngthu1995 <ngthu1995@gmail.com>
2023-03-11 21:56:13 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f914247ca3e76167623a5e2ceabf2b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea25aeeff28bc4ec5cd52afb5d114e74",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d68331cecc6ddb8bcea",
+    "url": "/portfolio/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ac58240acd7ff07883a1",
+    "url": "/portfolio/static/js/2.a42c0f2b.chunk.js"
+  },
+  {
+    "revision": "edfd5a8be4ec1fc32e761a9220464431",
+    "url": "/portfolio/static/js/2.a42c0f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d68331cecc6ddb8bcea",
+    "url": "/portfolio/static/js/main.1af32615.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
