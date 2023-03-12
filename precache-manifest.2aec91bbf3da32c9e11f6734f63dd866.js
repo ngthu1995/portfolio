ac8f93f0bf969dfd78d221169459f858fbd808e8
ngthu1995 <ngthu1995@gmail.com>
2023-03-11 22:29:13 -0500

    Updates

---
+++ precache-manifest.2aec91bbf3da32c9e11f6734f63dd866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc1bfc91ae990ab1456d1e50c90a5e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d68331cecc6ddb8bcea",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ac58240acd7ff07883a1",
+    "url": "/static/js/2.a42c0f2b.chunk.js"
+  },
+  {
+    "revision": "edfd5a8be4ec1fc32e761a9220464431",
+    "url": "/static/js/2.a42c0f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d68331cecc6ddb8bcea",
+    "url": "/static/js/main.1af32615.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
